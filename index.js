c73738970b0de163d35e5ea4c54ef3dadca2c733
Pramodh V <pramodhvenkatraman@gmail.com>
2022-09-27 01:32:40 +0530

    Connecting to db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require("mysql");
 
 const app = express();
 
@@ -6,6 +7,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: "testdb1.cp88egymboqh.ap-south-1.rds.amazonaws.com",
+    user: "root",
+    password: "host1234",
+    database: "testdb1"
+});
+
+connection.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
