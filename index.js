074f37eec14eec3edf5e6f4b5f2049977ad10386
Pramodh V <pramodhvenkatraman@gmail.com>
2022-10-01 14:47:09 +0530

    Adding tasks and code restructuring

--- index.js
+++ index.js
@@ -7,19 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connection = mysql.createConnection({
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
-});
-
-connection.connect(error => {
-    if (error) console.log("Error: Connection failed: ",error.stack);
-    else
-        console.log("Successfully connected to the database: ",process.env.RDS_DB_NAME);
-});
-
 app.get('/', (req, res) => {
     res.json({message:'Hello World!'});
 });
