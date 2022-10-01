adec8f25f48b8c517bcebcd82cfb6a129b1a1d11
Pramodh V <pramodhvenkatraman@gmail.com>
2022-10-01 15:01:38 +0530

    Adding tasks and code restructuring

--- db.js
+++ db.js
@@ -13,4 +13,9 @@ connection.connect(error => {
         console.log("Successfully connected to the database: ",process.env.RDS_DB_NAME);
 });
 
+connection.query('use sys', (error, results, fields) => {
+    if (error) console.log("Error: ",error);
+    else console.log("Successfully selected database: sys");
+});
+
 module.exports = connection;
